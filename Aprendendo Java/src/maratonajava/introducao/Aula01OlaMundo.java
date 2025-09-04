913e4f9c75b4d67935b1d313409055ab4b3505ac
joao <jvmmoraesdev@gmail.com>
2025-09-03 22:17:31 -0300

    mudança apenas no nome do arquivo para maior facilidade de indentificação de aulas

--- Aprendendo Java/src/maratonajava/introducao/Aula01OlaMundo.java
+++ Aprendendo Java/src/maratonajava/introducao/Aula01OlaMundo.java
@@ -4,7 +4,7 @@ package maratonajava.introducao;
 /**
  *  Isso é um comentario <b>JAVADOC</b>
  */
-public class OlaMundo {
+public class Aula01OlaMundo {
     public static void main(String[] args){
         System.out.println("Ola Mundo!");
     }
