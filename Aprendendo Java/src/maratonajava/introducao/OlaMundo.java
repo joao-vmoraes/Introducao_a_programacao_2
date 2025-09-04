a268e8bbbf1878b3826802a883961409ff08b652
joao <jvmmoraesdev@gmail.com>
2025-09-03 21:46:34 -0300

    adicionando um comentario javadoc

---
+++ Aprendendo Java/src/maratonajava/introducao/OlaMundo.java
@@ -0,0 +1,11 @@
+package maratonajava.introducao;
+
+
+/**
+ *  Isso é um comentario <b>JAVADOC</b>
+ */
+public class OlaMundo {
+    public static void main(String[] args){
+        System.out.println("Ola Mundo!");
+    }
+}
