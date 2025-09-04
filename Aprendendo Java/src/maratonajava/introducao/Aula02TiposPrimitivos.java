f60fee32e86fa7b9cfde0bc3bfbb7310633e8996
joao <jvmmoraesdev@gmail.com>
2025-09-04 11:13:29 -0300

    adição de tipos primitivos

--- Aprendendo Java/src/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ Aprendendo Java/src/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -7,6 +7,17 @@ public class Aula02TiposPrimitivos {
          */
 
         int age = 18;
+        long NumeroGrande = 10000000L;
+        double SalarioDouble = 2500;
+        float SalarioFloat = 2000.50F; //para indicar que é float,é necessario um 'f' no final do numero
+        byte IdadeByte = 10;
+        short IdadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'a';
+        String frase = "Olá, veja  a minha primeira String!";
+
         System.out.println("A idade é de " + age + " anos");
+        System.out.println(frase);
     }
 }
