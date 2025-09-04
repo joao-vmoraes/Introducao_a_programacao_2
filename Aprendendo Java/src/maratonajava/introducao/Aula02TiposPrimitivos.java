45cc64065515f9c70be3c8b265cf9e899129cdee
joao <jvmmoraesdev@gmail.com>
2025-09-03 22:16:56 -0300

    adicionado a aula 02, tipos primitivos

---
+++ Aprendendo Java/src/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+        /*
+        int, double, char, float, byte , short , long , boolean
+         */
+
+        int age = 18;
+        System.out.println("A idade é de " + age + " anos");
+    }
+}
